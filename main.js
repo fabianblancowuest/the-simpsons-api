41a71c7ff0ae40406e0f5ae6b7fb28e952a4d462
Fabián Blanco Wuest <fabianblancowuest@gmail.com>
2024-06-15 03:13:07 -0300

    video opening added

--- main.js
+++ main.js
@@ -13,3 +13,55 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log(links[i]);
     }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const opening = document.getElementById("opening");
+    const btnStart = document.getElementById("btn-start");
+    const video = document.getElementById("video");
+    const skipButton = document.getElementById("skip-button");
+    const sectionCharacters = document.getElementById("section-characters");
+    const title = document.getElementById("title");
+    const nav = document.getElementById("nav");
+    const footer = document.getElementById("footer");
+
+    let videoPlayedOnce = false;
+
+    btnStart.addEventListener("click", () => {
+        if (videoPlayedOnce) {
+            console.log(
+                "El video ya se ha reproducido. Mostrando contenido directamente."
+            );
+            showContent();
+        } else {
+            console.log("Iniciando la reproducción del video.");
+            sectionCharacters.classList.add("section-hidden");
+            video.play();
+            video.currentTime = 0;
+            btnStart.classList.add("section-hidden");
+            skipButton.style.display = "block";
+        }
+    });
+
+    skipButton.addEventListener("click", () => {
+        video.pause();
+        videoPlayedOnce = true;
+        showContent();
+    });
+
+    video.addEventListener("ended", () => {
+        videoPlayedOnce = true;
+        showContent();
+    });
+
+    function showContent() {
+        opening.classList.add("section-hidden");
+        sectionCharacters.classList.remove("section-hidden");
+        sectionCharacters.classList.add("section-show");
+        title.classList.remove("section-hidden");
+        title.classList.add("section-show");
+        footer.classList.remove("section-hidden");
+        footer.classList.add("section-show");
+        nav.classList.remove("section-hidden");
+        nav.classList.add("section-show");
+    }
+});
