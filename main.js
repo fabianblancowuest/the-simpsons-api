9a1043fe289dad42ab2953392196d7a3092bdd94
Fabián Blanco Wuest <fabianblancowuest@gmail.com>
2024-06-23 23:56:55 -0300

    improving the interface

--- main.js
+++ main.js
@@ -14,57 +14,57 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-    const opening = document.getElementById("opening");
-    const btnStart = document.getElementById("btn-start");
-    const video = document.getElementById("video");
-    const skipButton = document.getElementById("skip-button");
-    const sectionCharacters = document.getElementById("section-characters");
-    const title = document.getElementById("title");
-    const nav = document.getElementById("nav");
-    const footer = document.getElementById("footer");
-    const btnDetailContainer = document.getElementById("btn-detail-container");
+// document.addEventListener("DOMContentLoaded", function () {
+//     const opening = document.getElementById("opening");
+//     const btnStart = document.getElementById("btn-start");
+//     const video = document.getElementById("video");
+//     const skipButton = document.getElementById("skip-button");
+//     const sectionCharacters = document.getElementById("section-characters");
+//     const title = document.getElementById("title");
+//     const nav = document.getElementById("nav");
+//     const footer = document.getElementById("footer");
+//     const btnDetailContainer = document.getElementById("btn-detail-container");
 
-    let videoPlayedOnce = false;
+//     let videoPlayedOnce = false;
 
-    btnStart.addEventListener("click", () => {
-        if (videoPlayedOnce) {
-            console.log(
-                "El video ya se ha reproducido. Mostrando contenido directamente."
-            );
-            showContent();
-        } else {
-            console.log("Iniciando la reproducción del video.");
-            sectionCharacters.classList.add("section-hidden");
-            video.play();
-            video.currentTime = 0;
-            btnStart.classList.add("section-hidden");
-            skipButton.style.display = "block";
-        }
-    });
+//     btnStart.addEventListener("click", () => {
+//         if (videoPlayedOnce) {
+//             console.log(
+//                 "El video ya se ha reproducido. Mostrando contenido directamente."
+//             );
+//             showContent();
+//         } else {
+//             console.log("Iniciando la reproducción del video.");
+//             sectionCharacters.classList.add("section-hidden");
+//             video.play();
+//             video.currentTime = 0;
+//             btnStart.classList.add("section-hidden");
+//             skipButton.style.display = "block";
+//         }
+//     });
 
-    skipButton.addEventListener("click", () => {
-        video.pause();
-        videoPlayedOnce = true;
-        showContent();
-    });
+//     skipButton.addEventListener("click", () => {
+//         video.pause();
+//         videoPlayedOnce = true;
+//         showContent();
+//     });
 
-    video.addEventListener("ended", () => {
-        videoPlayedOnce = true;
-        showContent();
-    });
+//     video.addEventListener("ended", () => {
+//         videoPlayedOnce = true;
+//         showContent();
+//     });
 
-    function showContent() {
-        opening.classList.add("section-hidden");
-        sectionCharacters.classList.remove("section-hidden");
-        sectionCharacters.classList.add("section-show");
-        title.classList.remove("section-hidden");
-        title.classList.add("section-show");
-        footer.classList.remove("section-hidden");
-        footer.classList.add("section-show");
-        nav.classList.remove("section-hidden");
-        nav.classList.add("section-show");
-        btnDetailContainer.classList.remove("section-hidden");
-        btnDetailContainer.classList.add("section-show");
-    }
-});
+//     function showContent() {
+//         opening.classList.add("section-hidden");
+//         sectionCharacters.classList.remove("section-hidden");
+//         sectionCharacters.classList.add("section-show");
+//         title.classList.remove("section-hidden");
+//         title.classList.add("section-show");
+//         footer.classList.remove("section-hidden");
+//         footer.classList.add("section-show");
+//         nav.classList.remove("section-hidden");
+//         nav.classList.add("section-show");
+//         btnDetailContainer.classList.remove("section-hidden");
+//         btnDetailContainer.classList.add("section-show");
+//     }
+// });
